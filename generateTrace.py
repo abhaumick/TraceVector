1044d847efd5d71c3ab782ceea18ea9d986f0859
Abhishek Bhaumick <a.bhaumick@live.in>
2022-04-04 15:58:37 -0400

    Updated trace generator
    - uses TB and Warp level formatting

--- generateTrace.py
+++ generateTrace.py
@@ -11,23 +11,31 @@ import os
 import numpy
 
 filePath = 'trace.log'
+warpsPerTB = 10
 
 def getSizes(count : int) -> list :
-    sizes = numpy.random.randint(10, 100, count)
+    sizes = numpy.random.randint(10, 50, count)
     return(sizes)
 
 def encodeDummyTrace(sizes : list) -> str:
     traceList = []
-    for traceIdx in range(0, len(sizes)):
-        traceList.append('# Trace {} with {} records \n'.format(traceIdx, sizes[traceIdx]))
-        for idx in range(0, sizes[traceIdx]) :
-            traceList.append('- Line {}, {} \n'.format(traceIdx, idx))
+    for tbIdx in range(0, int(len(sizes) / warpsPerTB)):
+        traceList.append('#BEGIN_TB {} \n'.format(tbIdx, sizes[tbIdx]))
+        traceList.append('thread block = 2,3,4 \n')
+        for warpIdx in range(0, warpsPerTB) :
+            traceIdx = tbIdx * warpsPerTB + warpIdx
+            traceSize = sizes[traceIdx]
+            traceList.append('warp {}\n'.format(warpIdx))
+            traceList.append('insts = {}\n'.format(traceSize))
+            for idx in range(0, traceSize) :
+                traceList.append(' Line {},{} {} \n'.format(tbIdx, warpIdx, idx))
+        traceList.append('#END_TB {}\n'.format(tbIdx))
 
     traceString = ''.join(traceList)
     return traceString
 
 if __name__ == '__main__' :
-    sizes = getSizes(10)
+    sizes = getSizes(100)
     retString = encodeDummyTrace(sizes)
     with open(filePath, 'wt') as traceFile :
         traceFile.write(retString)
