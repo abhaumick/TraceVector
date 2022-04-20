2c8bef07a735f02e21ec05c92bd9eaf4b58c1e7b
Abhishek Bhaumick <a.bhaumick@live.in>
2022-04-20 19:18:06 -0400

    Updated trace generator, corrected newline discrepancy
    - added essential kernel_info lines and empty lines
    - updated instruction format
    - corrected file write to avoid `\n` conversion to `\n\r`

--- generateTrace.py
+++ generateTrace.py
@@ -11,33 +11,59 @@ import os
 import numpy
 
 filePath = 'trace.log'
+traceSize = 100
 warpsPerTB = 10
 
+minWarpSize = 10
+maxWarpSize = 100
+
 def getSizes(count : int) -> list :
-    sizes = numpy.random.randint(10, 50, count)
+    sizes = numpy.random.randint(minWarpSize, maxWarpSize, count)
     return(sizes)
 
 def encodeDummyTrace(sizes : list) -> str:
     traceList = []
-    for tbIdx in range(0, int(len(sizes) / warpsPerTB)):
+    numTB = int(len(sizes) / warpsPerTB)
+    
+    # File Header
+    traceList.append('-kernel name = dummy\n')
+    traceList.append('-kernel id = 1\n')
+    traceList.append('-grid dim = (1,4096,1)\n')
+    traceList.append('\n')
+
+    traceList.append("#traces format ="
+        " threadblock_x threadblock_y threadblock_z"
+        " warpid_tb PC mask dest_num [reg_dests] opcode"
+        " src_num [reg_srcs] mem_width [adrrescompress?] [mem_addresses]\n")
+    traceList.append('\n')
+
+
+    for tbIdx in range(0, numTB):
         traceList.append('#BEGIN_TB {} \n'.format(tbIdx, sizes[tbIdx]))
-        traceList.append('thread block = 2,3,4 \n')
+        traceList.append('\n')
+        traceList.append('thread block = 0,{},0 \n'.format(tbIdx))
+        traceList.append('\n')
         for warpIdx in range(0, warpsPerTB) :
             traceIdx = tbIdx * warpsPerTB + warpIdx
             traceSize = sizes[traceIdx]
-            traceList.append('warp {}\n'.format(warpIdx))
+            traceList.append('warp = {}\n'.format(warpIdx))
             traceList.append('insts = {}\n'.format(traceSize))
+            pc = 0
             for idx in range(0, traceSize) :
-                traceList.append(' Line {},{} {} \n'.format(tbIdx, warpIdx, idx))
+                traceList.append('{:04x} ffffffff 1 R0 instruction {}, {}, {} \n' \
+                    .format(pc, tbIdx, warpIdx, idx))
+                pc = pc + 0x10
+            traceList.append('\n')
         traceList.append('#END_TB {}\n'.format(tbIdx))
+        traceList.append('\n')
 
     traceString = ''.join(traceList)
     return traceString
 
 if __name__ == '__main__' :
-    sizes = getSizes(100)
+    sizes = getSizes(traceSize)
     retString = encodeDummyTrace(sizes)
-    with open(filePath, 'wt') as traceFile :
+    with open(filePath, 'wt', newline='') as traceFile :
         traceFile.write(retString)
         print('Wrote {} bytes to {}'.format(len(retString), filePath))
 
